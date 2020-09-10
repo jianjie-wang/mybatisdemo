3f4cca9fccd386abdb29e3bd6dc7e98014a7a2b0
jianjie.wang <wangjianjie>
2020-09-10 13:42:03 +0800

    update

---
+++ src/main/java/com/example/mybatisdemo/service/UserServise.java
@@ -0,0 +1,13 @@
+package com.example.mybatisdemo.service;
+
+import org.mapstruct.Mapper;
+
+/**
+ * @program: mybatisdemo
+ * @description:
+ * @author: WangJJ
+ * @create: 2020-09-10 13:40
+ **/
+@Mapper
+public class UserServise {
+}
